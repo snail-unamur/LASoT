41523f59ceb1d2e9bd0218d37106b12b158cdd21
SamuelVandePut <samuel.vandeput@student.unamur.be>
2022-06-30 22:16:22 +0200

    get paths and register main command to execute goals

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,38 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { MutationTestingProvider } from "./mutationTestingProvider";
+import { Goal } from "./mutationTestingProvider";
+import { Settings } from "./settings";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let NEXT_TERM_ID = 1;
+
+	let pomPath = Settings.getPomPath();
+	if(pomPath === undefined){
+		vscode.window.showErrorMessage("pom.xml not found.  Install maven plugin.")
+	}
+
+	let mavenExecutablePath = Settings.getMavenExecutablePath();
+	if(mavenExecutablePath === undefined){
+		vscode.window.showErrorMessage("Configure maven.executable.path in settings.json file.");
+	}
+	else{
+		mavenExecutablePath = mavenExecutablePath.concat('.cmd');
+	}
+
+    vscode.commands.registerCommand('mutationTesting.descartes.mutationCoverage', (node: Goal) => {
+        const terminal = vscode.window.createTerminal(`Ext Terminal #${NEXT_TERM_ID++}`);
+        terminal.show();
+        terminal.sendText(`& "${mavenExecutablePath}" ${node.command?.command} -f "${pomPath}"`);
+	});
 	
+	vscode.window.registerTreeDataProvider(
+		'mutationTesting',
+		new MutationTestingProvider()
+	);
 }
 
 // this method is called when your extension is deactivated

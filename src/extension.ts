b58c61411d22196a96ba22fc25f3eafaa918ab6e
SamuelVandePut <samuel.vandeput@student.unamur.be>
2022-06-30 22:49:06 +0200

    subscription of wizard command

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { commands } from 'vscode';
 import { MutationTestingProvider } from "./explorer/mutationTestingProvider";
 import { Goal } from "./explorer/mutationTestingProvider";
+import { multiStepInput } from './multiStepInput';
 import { Settings } from "./settings";
 
 // this method is called when your extension is activated
@@ -33,6 +35,10 @@ export function activate(context: vscode.ExtensionContext) {
 		'mutationTesting',
 		new MutationTestingProvider()
 	);
+
+	context.subscriptions.push(commands.registerCommand('lasot.wizard', async () => {
+		multiStepInput(context).catch(console.error);
+	}));
 }
 
 // this method is called when your extension is deactivated

1315cb10e358d178c3ce7b0633f06a8e8de73099
SamuelVandePut <samuel.vandeput@student.unamur.be>
2022-06-30 22:28:51 +0200

    correct command configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { MutationTestingProvider } from "./mutationTestingProvider";
-import { Goal } from "./mutationTestingProvider";
+import { MutationTestingProvider } from "./explorer/mutationTestingProvider";
+import { Goal } from "./explorer/mutationTestingProvider";
 import { Settings } from "./settings";
 
 // this method is called when your extension is activated
@@ -23,7 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
 		mavenExecutablePath = mavenExecutablePath.concat('.cmd');
 	}
 
-    vscode.commands.registerCommand('mutationTesting.descartes.mutationCoverage', (node: Goal) => {
+    vscode.commands.registerCommand('executeGoal', (node: Goal) => {
         const terminal = vscode.window.createTerminal(`Ext Terminal #${NEXT_TERM_ID++}`);
         terminal.show();
         terminal.sendText(`& "${mavenExecutablePath}" ${node.command?.command} -f "${pomPath}"`);

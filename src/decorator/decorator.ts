6208cb2773e20536a2287d784119f6c66dfd3c87
SamuelVandePut <samuel.vandeput@student.unamur.be>
2022-07-20 21:18:55 +0200

    Update decorator.ts

--- src/decorator/decorator.ts
+++ src/decorator/decorator.ts
@@ -1,7 +1,8 @@
 import * as vscode from 'vscode';
 import { MarkdownString } from 'vscode';
 import { DescartesMethod, DescartesState } from '../descartesState';
-import { Hint, ReneriState, Survivor } from '../reneriState';
+import { Hint, ReneriState, SignaledMethod } from '../reneriState';
+import { Settings } from '../settings';
 
 export class Decorator {
 
@@ -58,7 +59,7 @@ export class Decorator {
     
 		const decorationOptions: vscode.DecorationOptions[] = [];
 
-		for(const survivor of this.reneriState.testsObservation.survivors){	
+		for(const survivor of this.reneriState.testsObservation.signaledMethods){	
 			for(const hint of survivor.hints){
 				if(this.activeEditor.document.uri.fsPath.toLocaleLowerCase() === hint.location.file.toLowerCase()){
 					const decoration = this.generateTestDecoration(hint,survivor);
@@ -67,33 +68,30 @@ export class Decorator {
 			}
 		}
 		
-		for(const survivor of this.reneriState.methodsObservation.survivors){	
-			for(const diff of survivor.diffs){
-				if(!diff.pointcut.match('^.*history.*$')){
-					const descartesMutation = this.descartesState.descartesReports.mutationReport.mutations.filter(m => 
-						m.method.class === survivor.mutation.class 
-						&& m.method.package === survivor.mutation.package
-						&& m.method.name === survivor.mutation.method);
-					const descartesMethod = this.descartesState.descartesReports.methodReport.methods.filter(m => 
-						m.class === survivor.mutation.class 
-						&& m.package === survivor.mutation.package
-						&& m.name === survivor.mutation.method);
-					if(descartesMutation && descartesMethod){	
-						const f = await vscode.workspace.findFiles('**/src/**/' + descartesMutation[0].file);
-						if(f){
-							if(this.activeEditor.document.uri.fsPath.toLocaleLowerCase() === f[0].fsPath.toLowerCase()){
-								let l = descartesMutation[0].line;
-								while(!this.activeEditor.document.lineAt(l).text.match('^.*' + descartesMutation[0].method.name + '.*$')){
-									l--;
-									if(l === descartesMutation[0].line-10){
-										break;
-									}
-								}
-								const range = this.activeEditor.document.lineAt(l).range;
-								const decoration = this.generateMethodDecoration(descartesMethod[0],range);
-								decorationOptions.push(decoration);
+		for(const signaledMethod of this.reneriState.methodsObservation.signaledMethods){	
+			//  Pour chaque méthode signalée, affiche les informations des reneri et descartes
+			const descartesMutation = this.descartesState.descartesReports.mutationReport.mutations.filter(m => 
+				m.method.class === signaledMethod.mutation.class 
+				&& m.method.package === signaledMethod.mutation.package
+				&& m.method.name === signaledMethod.mutation.method);
+			const descartesMethod = this.descartesState.descartesReports.methodReport.methods.filter(m => 
+				m.class === signaledMethod.mutation.class 
+				&& m.package.split('/').join('.') === signaledMethod.mutation.package
+				&& m.name === signaledMethod.mutation.method);
+			if(descartesMutation && descartesMethod){	
+				const f = await vscode.workspace.findFiles('**/src/**/' + descartesMutation[0].file);
+				if(f){
+					if(this.activeEditor.document.uri.fsPath.toLocaleLowerCase() === f[0].fsPath.toLowerCase()){
+						let l = descartesMutation[0].line;
+						while(!this.activeEditor.document.lineAt(l).text.match('^.*' + descartesMutation[0].method.name + '.*$')){
+							l--;
+							if(l === descartesMutation[0].line-10){
+								break;
 							}
 						}
+						const range = this.activeEditor.document.lineAt(l).range;
+						const decoration = this.generateMethodDecoration(descartesMethod[0],range);
+						decorationOptions.push(decoration);
 					}
 				}
 			}
@@ -127,7 +125,7 @@ export class Decorator {
 		return { range: range, hoverMessage: hoverMessage };
 	}
 
-	generateTestDecoration(hint: Hint, survivor: Survivor) : vscode.DecorationOptions {
+	generateTestDecoration(hint: Hint, survivor: SignaledMethod) : vscode.DecorationOptions {
 			const from = new vscode.Position(hint.location.from.line-1, hint.location.from.column-1);
 			const to = new vscode.Position(hint.location.to.line-1, hint.location.to.column);
 			const range: vscode.Range = new vscode.Range(from , to);
@@ -136,7 +134,7 @@ export class Decorator {
 	}
 
 
-	generateTestHoverMessage(survivor: Survivor, hint: Hint) : MarkdownString{
+	generateTestHoverMessage(survivor: SignaledMethod, hint: Hint) : MarkdownString{
 		const diff = survivor.diffs.find(d => d.pointcut === hint.pointcut);
 		let markDownString: MarkdownString = new MarkdownString();
 		markDownString.appendMarkdown( 
@@ -159,19 +157,36 @@ export class Decorator {
 		let markDownString: MarkdownString = new MarkdownString();
 		markDownString.appendMarkdown(`<p><em>This method is ${descartesMethod.classification}</em></p>`);
 		for(const mutation of descartesMethod.mutations){
+
+			let description = "";
+			if(!mutation.mutator.match('/^null|void|empty|new|argument|optional|this/g')){
+				description = Settings.getMutatorDescription("constant") + mutation.mutator;
+			}
+			else{
+				description = Settings.getMutatorDescription(mutation.mutator);
+			}
+
 			if(mutation.status === 'SURVIVED'){
+				let mutationTestsString: string = "";
+				for(const test of mutation.tests){
+					mutationTestsString += "<code>" + test + "</code><br>";
+				}
 				markDownString.appendMarkdown(
 				`<p><span style="color:#00BE83;">Undetected</span> mutation :</p>
 				<ul>  
-				  <li><span style="color:#00BE83;"> Mutator</span> : ${mutation.mutator} </li>  
-				  <li><span style="color:#00BE83;"> Tests run</span> : <code>${mutation.tests}</code></li>
+				  <li><span style="color:#00BE83;"> Mutator</span> : ${mutation.mutator} <br><code>${description}</code></li>  
+				  <li><span style="color:#00BE83;"> Tests run</span> : ${mutationTestsString}</li>
 				</ul>`);
 			}
 			if(mutation.status === "KILLED"){
+				let mutationTestsString: string = "";
+				for(const test of mutation['killing-tests']){
+					mutationTestsString += "<code>" + test + "</code><br>";
+				}
 				markDownString.appendMarkdown(
 				`<p><span style="color:#00BE83;">Killed</span> mutation :</p>
 				<ul>  
-				  <li><span style="color:#00BE83;"> Mutator</span> : ${mutation.mutator}  </li>
+				  <li><span style="color:#00BE83;"> Mutator</span> : ${mutation.mutator}  <br><code>${description}</code></li>
 				  <li><span style="color:#00BE83;"> Killing tests</span> : <code>${mutation['killing-tests']}</code>  </li>
 				</ul>`);	
 			}
